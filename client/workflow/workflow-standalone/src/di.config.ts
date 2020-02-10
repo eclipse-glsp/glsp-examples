c70816bccc65023f59c7f70ea0f98d986e385f0f
Tobias Ortmayr <tortmayr@eclipsesource.com>
2020-02-10 10:34:40 +0100

    Fix css in standalone workflow example

--- client/workflow/workflow-standalone/src/di.config.ts
+++ client/workflow/workflow-standalone/src/di.config.ts
@@ -13,6 +13,8 @@
  *
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  ********************************************************************************/
+import "../css/diagram.css";
+
 import { createWorkflowDiagramContainer } from "@eclipse-glsp-examples/workflow-sprotty/lib";
 import { GLSPWebsocketDiagramServer, registerDefaultTools } from "@eclipse-glsp/client/lib";
 import { Container } from "inversify";
