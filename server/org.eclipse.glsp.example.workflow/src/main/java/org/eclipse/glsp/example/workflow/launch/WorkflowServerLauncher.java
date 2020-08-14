9b49550342cb91449c85130e0e663e94a9d31202
Tobias Ortmayr <tortmayr@eclipsesource.com>
2020-08-14 12:58:53 +0200

    Address review feedback

--- server/org.eclipse.glsp.example.workflow/src/main/java/org/eclipse/glsp/example/workflow/launch/WorkflowServerLauncher.java
+++ server/org.eclipse.glsp.example.workflow/src/main/java/org/eclipse/glsp/example/workflow/launch/WorkflowServerLauncher.java
@@ -13,16 +13,14 @@
  *
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  ********************************************************************************/
-package org.eclipse.glsp.example.workflow;
+package org.eclipse.glsp.example.workflow.launch;
 
 import java.io.IOException;
 
-import org.apache.commons.cli.Option;
-import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.eclipse.elk.alg.layered.options.LayeredMetaDataProvider;
+import org.eclipse.glsp.example.workflow.WorkflowGLSPModule;
 import org.eclipse.glsp.layout.ElkLayoutEngine;
-import org.eclipse.glsp.server.launch.CLIParser;
 import org.eclipse.glsp.server.launch.DefaultGLSPServerLauncher;
 import org.eclipse.glsp.server.launch.GLSPServerLauncher;
 import org.eclipse.glsp.server.utils.LaunchUtil;
@@ -34,32 +32,23 @@ public final class WorkflowServerLauncher {
    @SuppressWarnings("uncommentedmain")
    public static void main(final String[] args) {
       String processName = "WorkflowExampleGlspServer";
-      Options cliOptions = CLIParser.getDefaultCLIOptions();
-      cliOptions.addOption(new Option("w", "websocket", false, "Use websocket launcher instead of default"));
-
       try {
-         CLIParser parser = new CLIParser(args, cliOptions, processName);
-         if (parser.optionExists("h")) {
-            parser.printHelp();
-            System.exit(0);
-         }
-
-         LaunchUtil.configureLogger(parser);
+         WorkflowCLIParser parser = new WorkflowCLIParser(args, processName);
+         LaunchUtil.configure(parser);
          ElkLayoutEngine.initialize(new LayeredMetaDataProvider());
 
          int port = parser.parsePort();
 
-         GLSPServerLauncher launcher = parser.optionExists("w")
+         GLSPServerLauncher launcher = parser.isWebsocket()
             ? new WebsocketServerLauncher(new WorkflowGLSPModule(), "/workflow")
             : new DefaultGLSPServerLauncher(new WorkflowGLSPModule());
 
          launcher.start("localhost", port);
 
       } catch (ParseException | IOException ex) {
-         System.out.println("Server start failed!");
          ex.printStackTrace();
          System.out.println();
-         CLIParser.printHelp(processName, cliOptions);
+         LaunchUtil.printHelp(processName, WorkflowCLIParser.getDefaultOptions());
       }
    }
 }
