f067e6c79583d7cc711c26b18cc6efbdb32308c4
Philip Langer <planger@eclipsesource.com>
2022-12-09 20:35:57 +0000

    Solution 5: Add custom action

--- project-templates/node-json-theia/glsp-client/tasklist-theia/src/browser/tasklist-contribution.ts
+++ project-templates/node-json-theia/glsp-client/tasklist-theia/src/browser/tasklist-contribution.ts
@@ -14,25 +14,39 @@
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  ********************************************************************************/
 
-import { GLSPContextMenu } from '@eclipse-glsp/theia-integration';
+import { DefaultTypes } from '@eclipse-glsp/client';
+import { GLSPCommandHandler, GLSPContextMenu } from '@eclipse-glsp/theia-integration';
 import { CommandContribution, CommandRegistry, MenuContribution, MenuModelRegistry } from '@theia/core';
 import { ApplicationShell } from '@theia/core/lib/browser';
 import { inject, injectable } from '@theia/core/shared/inversify';
+import { MyCustomAction } from './actions';
 
 @injectable()
 export class TaskListContribution implements CommandContribution, MenuContribution {
     @inject(ApplicationShell) protected readonly shell: ApplicationShell;
 
     static HELLO_WORLD_COMMAND = { id: 'glsp-say-hello', label: 'Say hello from GLSP' };
+    static MY_CUSTOM_ACTION_COMMAND = { id: 'glsp-custom-action', label: 'My Custom Action' };
 
     registerCommands(commands: CommandRegistry): void {
         commands.registerCommand(TaskListContribution.HELLO_WORLD_COMMAND, { execute: () => console.log('Hello world') });
+        commands.registerCommand(
+            TaskListContribution.MY_CUSTOM_ACTION_COMMAND,
+            new GLSPCommandHandler(this.shell, {
+                actions: context => [MyCustomAction.create(context.selectedElements[0].id)],
+                isEnabled: context => context.selectedElements.length === 1 && context.selectedElements[0].type === DefaultTypes.NODE
+            })
+        );
     }
 
     registerMenus(menus: MenuModelRegistry): void {
         menus.registerMenuAction(GLSPContextMenu.MENU_PATH.concat('edit'), {
             commandId: TaskListContribution.HELLO_WORLD_COMMAND.id,
             label: 'Menu: Say hello from GLSP'
+        });
+        menus.registerMenuAction(GLSPContextMenu.MENU_PATH.concat('edit'), {
+            commandId: TaskListContribution.MY_CUSTOM_ACTION_COMMAND.id,
+            label: TaskListContribution.MY_CUSTOM_ACTION_COMMAND.label
         });
     }
 }
