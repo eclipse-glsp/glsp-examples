defaea9394096d405ef78ed421cfff99c1e77a1f
Tobias Ortmayr <tortmayr@eclipsesource.com>
2023-09-22 14:23:20 +0200

    [WIP] Restructuring of node-based project templates
    WIP demonstration of the planned restructuring for the node-based project templates using the`node-json-theia` template
    https://github.com/eclipse-glsp/glsp/issues/1012

---
+++ project-templates/node-json-theia/tasklist-theia/src/common/tasklist-language.ts
@@ -0,0 +1,25 @@
+/********************************************************************************
+ * Copyright (c) 2022 EclipseSource and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License v. 2.0 are satisfied:
+ * -- GNU General Public License, version 2 with the GNU Classpath Exception
+ * which is available at https://www.gnu.org/software/classpath/license.html
+ * -- MIT License which is available at https://opensource.org/license/mit.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0 OR MIT
+ ********************************************************************************/
+
+import { GLSPDiagramLanguage } from '@eclipse-glsp/theia-integration/lib/common';
+
+export const TaskListLanguage: GLSPDiagramLanguage = {
+    contributionId: 'tasklist',
+    label: 'TaskList Diagram',
+    diagramType: 'tasklist-diagram',
+    fileExtensions: ['.tasklist']
+};
